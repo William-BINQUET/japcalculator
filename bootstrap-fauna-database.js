9c84cb742bef0345419ef3bb49ca78093ee6f539
William Binquet <william.binquet@student-cs.fr>
2021-10-15 14:02:28 +0200

    Changed pipeline to take eslint into account

--- bootstrap-fauna-database.js
+++ bootstrap-fauna-database.js
@@ -1,33 +1,33 @@
 /* bootstrap database in your FaunaDB account */
-const readline = require('readline')
-const faunadb = require('faunadb')
-const chalk = require('chalk')
-const insideNetlify = insideNetlifyBuildContext()
-const q = faunadb.query
+const readline = require("readline");
+const faunadb = require("faunadb");
+const chalk = require("chalk");
+const insideNetlify = insideNetlifyBuildContext();
+const q = faunadb.query;
 
-console.log(chalk.cyan('Creating your FaunaDB Database...\n'))
+console.log(chalk.cyan("Creating your FaunaDB Database...\n"));
 
 // 1. Check for required enviroment variables
 if (!process.env.FAUNADB_SECRET) {
-  console.log(chalk.yellow('Required FAUNADB_SECRET enviroment variable not found.'))
+  console.log(chalk.yellow("Required FAUNADB_SECRET enviroment variable not found."));
   if (insideNetlify) {
-    console.log(`Visit https://app.netlify.com/sites/YOUR_SITE_HERE/settings/deploys`)
-    console.log('and set a `FAUNADB_SECRET` value in the "Build environment variables" section')
-    process.exit(1)
+    console.log("Visit https://app.netlify.com/sites/YOUR_SITE_HERE/settings/deploys");
+    console.log("and set a `FAUNADB_SECRET` value in the \"Build environment variables\" section");
+    process.exit(1);
   }
   // Local machine warning
   if (!insideNetlify) {
-    console.log()
-    console.log('You can create fauna DB keys here: https://dashboard.fauna.com/db/keys')
-    console.log()
-    ask(chalk.bold('Enter your faunaDB server key'), (err, answer) => {
+    console.log();
+    console.log("You can create fauna DB keys here: https://dashboard.fauna.com/db/keys");
+    console.log();
+    ask(chalk.bold("Enter your faunaDB server key"), (err, answer) => {
       if (!answer) {
-        console.log('Please supply a faunaDB server key')
-        process.exit(1)
+        console.log("Please supply a faunaDB server key");
+        process.exit(1);
       }
       createFaunaDB(process.env.FAUNADB_SECRET).then(() => {
-        console.log('Database created')
-      })
+        console.log("Database created");
+      });
     });
   }
 }
@@ -35,13 +35,13 @@ if (!process.env.FAUNADB_SECRET) {
 // Has var. Do the thing
 if (process.env.FAUNADB_SECRET) {
   createFaunaDB(process.env.FAUNADB_SECRET).then(() => {
-    console.log('Database created')
-  })
+    console.log("Database created");
+  });
 }
 
 /* idempotent operation */
 function createFaunaDB(key) {
-  console.log('Create the database!')
+  console.log("Create the database!");
   const client = new faunadb.Client({
     secret: key
   });
@@ -53,14 +53,14 @@ function createFaunaDB(key) {
         q.Create(q.Ref("indexes"), {
           name: "all_todos",
           source: q.Ref("classes/todos")
-        }))
+        }));
     }).catch((e) => {
       // Database already exists
-      if (e.requestResult.statusCode === 400 && e.message === 'instance not unique') {
-        console.log('DB already exists')
-        throw e
+      if (e.requestResult.statusCode === 400 && e.message === "instance not unique") {
+        console.log("DB already exists");
+        throw e;
       }
-    })
+    });
 }
 
 
@@ -69,9 +69,9 @@ function createFaunaDB(key) {
 // Test if inside netlify build context
 function insideNetlifyBuildContext() {
   if (process.env.DEPLOY_PRIME_URL) {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
 
 // Readline util
@@ -80,7 +80,7 @@ function ask(question, callback) {
     input: process.stdin,
     output: process.stdout
   });
-  rl.question(question + '\n', function(answer) {
+  rl.question(question + "\n", function(answer) {
     rl.close();
     callback(null, answer);
   });
